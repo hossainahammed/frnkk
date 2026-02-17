eea14973d9fd30ed87ac5f229ad6e1a7a63b15c5
hossainahammed <hossainahammed627@gmail.com>
2026-02-17 14:18:16 +0600

    Update main.dart changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:frnkk/controllers/onboarding_controller.dart';
-import 'package:frnkk/controllers/profile_controller.dart';
-import 'package:frnkk/ui/screens/artists_profile/artists_profile_controller.dart';
-import 'package:frnkk/controllers/create_new_password_controller.dart';
-import 'package:frnkk/controllers/forgot_password_controller.dart';
+
 import 'package:frnkk/controllers/home_controller.dart';
-import 'package:frnkk/ui/screens/login/login_controller.dart';
-import 'package:frnkk/controllers/otp_controller.dart';
-import 'package:frnkk/controllers/signup_controller.dart';
+
 import 'package:frnkk/widgets/bottom_navigation_bar/navigation_controller.dart';
 import 'package:get/get.dart';
 
@@ -27,23 +21,3 @@ void main() {
     ),
   );
 }
-// void main() {
-//   //Get.put(OnboardingController());
-//   Get.lazyPut(() => OnboardingController());
-//   Get.put(LoginController());
-//   Get.lazyPut(() => SignUpController());
-//   Get.lazyPut(() => ForgotPasswordController());
-//   Get.lazyPut(() => CreateNewPasswordController());
-//   Get.lazyPut(() => OtpController());
-//   Get.lazyPut(() => NavigationController());
-//   Get.lazyPut(() => HomeController());
-//   Get.lazyPut(() => ArtistProfileController());
-//   Get.lazyPut(() => ProfileController());
-//
-//   runApp(
-//     DevicePreview(
-//       enabled: false,
-//       builder: (context) => const frnkApp(),
-//     ),
-//   );
-// }
