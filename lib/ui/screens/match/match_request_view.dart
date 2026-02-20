1409386b01e675cc861b71bb32983730e976662b
Mushfiqur Rahman <mushfiqurrahman.dm@gmail.com>
2026-02-20 09:14:32 +0600

    the cards of match screen and message screen seems identical now

--- lib/ui/screens/match/match_request_view.dart
+++ lib/ui/screens/match/match_request_view.dart
@@ -1,9 +1,9 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:frnkk/ui/screens/home/home_view.dart';
 import 'package:frnkk/widgets/bottom_navigation_bar/navigation_controller.dart';
 import 'package:get/get.dart';
 import 'package:frnkk/widgets/app_background.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class MatchRequestPage extends StatelessWidget {
   const MatchRequestPage({super.key});
@@ -23,17 +23,18 @@ class MatchRequestPage extends StatelessWidget {
             leading: IconButton(
               icon: const Icon(Icons.arrow_back, color: Colors.white),
               // onPressed: () => Navigator.pop(context),
-               // onPressed: () => Get.to(HomeScreen()),
+              // onPressed: () => Get.to(HomeScreen()),
               onPressed: () {
                 // Switch back to the home tab (index 0) instead of navigating
                 final navController = Get.find<NavigationController>();
-                navController.changeTabIndex(0);  // Assumes changeTabIndex is the method in NavigationController
+                navController.changeTabIndex(
+                  0,
+                ); // Assumes changeTabIndex is the method in NavigationController
               },
-
             ),
-            title: const Text(
+            title: Text(
               "Match",
-              style: TextStyle(
+              style: GoogleFonts.poltawskiNowy(
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
               ),
@@ -68,90 +69,75 @@ class MatchList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
-      padding: const EdgeInsets.all(20),
+      padding: const EdgeInsets.symmetric(vertical: 20),
       itemCount: 8, // Replace with your controller logic
-      separatorBuilder: (context, index) => const SizedBox(height: 20),
+      separatorBuilder: (context, index) => const SizedBox(height: 10),
       itemBuilder: (context, index) {
-        return Row(
-          children: [
-            // Profile Image with Online Dot
-            Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      width: 2,
-                    ),
-                  ),
-                  child: const CircleAvatar(
-                    radius: 25,
-                    backgroundImage: AssetImage(
-                      'assets/images/artists_profile_images/selena.png',
-                    ), // Add your image
-                  ),
-                ),
-                Positioned(
-                  right: 2,
-                  top: 2,
-                  child: Container(
-                    width: 10,
-                    height: 10,
-                    decoration: BoxDecoration(
-                      color: Colors.blueAccent, // Blue dot from image
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 255, 255, 255),
-                        width: 2,
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            const SizedBox(width: 15),
+        return _buildMatchTile();
+      },
+    );
+  }
 
-            // User Info
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Salena Gomez",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Last Activity",
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 14,
-                        ),
-                      ),
-                      Text(
-                        "12 Aug 2022 - 12:55 am",
-                        style: TextStyle(
-                          color: const Color.fromARGB(255, 255, 255, 255),
-                          fontSize: 12,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+  Widget _buildMatchTile() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20),
+      decoration: BoxDecoration(
+        color: Colors.transparent, // Match unselected state of MessageList
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ListTile(
+        minTileHeight: 63,
+        leading: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+              child: const CircleAvatar(
+                radius: 25,
+                backgroundImage: AssetImage(
+                  'assets/images/artists_profile_images/selena.png',
+                ),
               ),
             ),
-
-            // Date/Time
+            Positioned(
+              right: 2,
+              top: 1,
+              child: Container(
+                height: 12,
+                width: 12,
+                decoration: BoxDecoration(
+                  color: Colors.blueAccent,
+                  shape: BoxShape.circle,
+                  border: Border.all(color: Colors.white, width: 2),
+                ),
+              ),
+            ),
           ],
-        );
-      },
+        ),
+        title: const Text(
+          "Salena Gomez",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        subtitle: const Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Last Activity",
+              style: TextStyle(color: Colors.white, fontSize: 14),
+            ),
+            Text(
+              "12 Aug 2022 - 12:55 am",
+              style: TextStyle(color: Colors.white, fontSize: 12),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
